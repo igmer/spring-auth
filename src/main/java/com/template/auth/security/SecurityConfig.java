8a9689da324da7121380d316a8bfcfdbef475df8
igmer <igmerdz@gmail.com>
2022-12-13 22:39:20 -0600

    Adding cors to use with angular

--- src/main/java/com/template/auth/security/SecurityConfig.java
+++ src/main/java/com/template/auth/security/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +36,17 @@ private final UserDetailService userDetailService;
                 .antMatchers("/**/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
+                .and()
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.addAllowedOrigin("*");
+                        return config;
+                    }
+                })
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
